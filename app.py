e0ec2785f2d4ccb5e051ff0affdf7b4859e4ff26
Kacper Dziedzic <kacper.dziedzic@wp.pl>
2021-05-06 10:34:39 +0200

    Added Catalog Tasks process

--- app.py
+++ app.py
@@ -4,28 +4,52 @@ import pymsteams
 
 
 myTeamsMessage = pymsteams.connectorcard("#paste your incoming webhook url here#")
+myMessageSection = pymsteams.cardsection()
+myTeamsMessage.addSection(myMessageSection)
+myTeamsMessage.color("#fab011")
 
-url = '#Servicenow API link#'
+url_inc = '#Servicenow API link#'
+url_task = '#Servicenow API link#'
 
 user = '#Servicenow login#'
 pwd = '#Servicenow password#'
 
-while True:
-    response = requests.get(url, auth=(user, pwd))
-    if response.status_code != 200: 
-        print('Lack of assigned tickets!')
-        print('-------END OF CYCLE-------')
-        time.sleep(300)
-    else:
-        data = response.json()
+def inc():
+    while True:
+        response_inc = requests.get(url_inc, auth=(user, pwd))
+        if response_inc.status_code != 200: 
+            print('Lack of assigned Incidents!')
+            print('-------END OF INC CYCLE-------')
+            task()
+            time.sleep(300)
+        else:
+            data = response_inc.json()
+            for i in data['result']:
+                myTeamsMessage.text('#Type your text message here')
+                myTeamsMessage.send()
+                print('{} has been posted'.format(i['number']))
+                time.sleep(5)
+            print('-------END OF INC CYCLE-------')
+            task()
+            time.sleep(300)
 
-        for i in data['result']:
-            myTeamsMessage.text('<h1><center><a href="{}">{}</a> has been assigned to your queue!</center></h1>'.format(i['sys_id'], i['number']))
-            myTeamsMessage.send()
-            print('{} has been posted'.format(i['number']))
-            time.sleep(5)
-        print('-------END OF CYCLE-------')
-        time.sleep(300)
+def task():
+    while True:
+        response_task = requests.get(url_task, auth=(user, pwd))
+        if response_task.status_code != 200: 
+            print('Lack of assigned Catalog Tasks!')
+            print('-------END OF TASK CYCLE-------')
+            time.sleep(300)
+            inc()
+        else:
+            data_task = response_task.json()
+            for i in data_task['result']:
+                myTeamsMessage.text('#Type your text message here')
+                myTeamsMessage.send()
+                print('{} has been posted'.format(i['number']))
+                time.sleep(5)
+            print('-------END OF TASK CYCLE-------')
+            time.sleep(300)
+            inc()
 
-
-
+inc()
